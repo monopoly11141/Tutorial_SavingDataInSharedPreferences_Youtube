7fb06a48b9d22f60eaf1a7c4e28ea9c6932fbc37
monopoly11141 <monopoly1114@vt.edu>
2023-03-28 12:47:21 +0900

    +Shared Preferences
    Allowed users to save and load data using shared preferences.

--- app/src/main/java/com/example/tutorial_savingdatainsharedpreferences_youtube/MainActivity.kt
+++ app/src/main/java/com/example/tutorial_savingdatainsharedpreferences_youtube/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.tutorial_savingdatainsharedpreferences_youtube
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.example.tutorial_savingdatainsharedpreferences_youtube.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val sharedPref = getSharedPreferences("myPref", MODE_PRIVATE)
+        val editor = sharedPref.edit()
+
+        binding.btnSave.setOnClickListener {
+            val name = binding.etName.text.toString()
+            val age = binding.etAge.text.toString().toInt()
+            val isAdult = binding.cbAdult.isChecked
+
+            editor.apply {
+                putString("name", name)
+                putInt("age", age)
+                putBoolean("isAdult", isAdult)
+                apply()
+            }
+        }
+
+        binding.btnLoad.setOnClickListener {
+            val name = sharedPref.getString("name", null)
+            val age = sharedPref.getInt("age", 0)
+            val isAdult = sharedPref.getBoolean("isAdult", false)
+
+            binding.etName.setText(name)
+            binding.etAge.setText(age.toString())
+            binding.cbAdult.isChecked = isAdult
+
+        }
+
     }
 }
